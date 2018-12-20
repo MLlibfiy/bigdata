0df79bdf8dc2f359e895000eebc4a9e6a44d9577
qinxiao <qinx@chinatelecom.cn>
2018-12-20 22:39:32 +0800

    增加删除索引协处理器

--- src/main/java/com/shujia/hbase/IndexHBaseCoprocessor.java
+++ src/main/java/com/shujia/hbase/IndexHBaseCoprocessor.java
@@ -8,6 +8,9 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.RegexStringComparator;
+import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -29,6 +32,53 @@ public class IndexHBaseCoprocessor extends BaseRegionObserver {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     *
+     * 在对表执行删除操作时执行此方法
+     *
+     */
+    @Override
+    public void preDelete(ObserverContext<RegionCoprocessorEnvironment> e, Delete delete, WALEdit edit, Durability durability) throws IOException {
+
+        String row = Bytes.toString(delete.getRow());
+
+        Scan scan = new Scan();
+
+        RegexStringComparator regexStringComparator = new RegexStringComparator(".*" + row);
+
+        RowFilter rowFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, regexStringComparator);
+
+        scan.setFilter(rowFilter);
+
+        ResultScanner scanner = table.getScanner(scan);
+
+        Result next;
+
+
+        while ((next = scanner.next()) != null) {
+
+            String gender = null;
+            String clazz = null;
+
+            List<Cell> cells = next.listCells();
+            for (Cell cell : cells) {
+                if ("info".equals(Bytes.toString(CellUtil.cloneFamily(cell)))) {
+                    if ("gender".equals(Bytes.toString(CellUtil.cloneQualifier(cell)))) {
+                        gender = Bytes.toString(CellUtil.cloneValue(cell));
+                    } else if ("clazz".equals(Bytes.toString(CellUtil.cloneQualifier(cell)))) {
+                        clazz = Bytes.toString(CellUtil.cloneValue(cell));
+                    }
+                }
+            }
+            if(clazz!=null && gender!=null){
+                String rowkey = ContentUtil.getClazzIndex(clazz) + "_" + ContentUtil.getGenderIndex(gender);
+
+                //删除索引表里的索引
+                table.delete(new Delete(rowkey.getBytes()));
+            }
+        }
     }
 
     /**
